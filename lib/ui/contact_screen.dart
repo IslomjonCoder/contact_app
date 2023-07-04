dbc0a25ab355a8f5b0544dabc2c678021d9b7443
islom <islomjon20010930@gmail.com>
2023-07-04 17:36:23 +0500

    second commit

--- lib/ui/contact_screen.dart
+++ lib/ui/contact_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled12/models/contact.dart';
 import 'package:untitled12/reposityory/contact_repository.dart';
 import 'package:untitled12/ui/contact_add.dart';
+import 'package:untitled12/ui/contact_detail.dart';
 import 'package:untitled12/ui/widgets/custom_searchbar.dart';
 
 class ContactScreen extends StatefulWidget {
@@ -12,12 +13,12 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
-  List<Contact> contacts = [];
+  // List<Contact> contacts = [];
 
   _loadContacts() async {
     List<Contact> newContacts = await DatabaseHelper.getContacts();
     setState(() {
-      contacts = newContacts;
+      // contacts = newContacts;
     });
     print('Contacts loaded');
   }
@@ -34,22 +35,47 @@ class _ContactScreenState extends State<ContactScreen> {
       appBar: AppBar(
         title: Text('Contacts'),
         actions: [
-          IconButton(
-              onPressed: () {
-                // showSearch(context: context, delegate: CustomSearchDelegate());
-                // DatabaseHelper.deleteContacts();
-                // _loadContacts();
-              },
-              icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text("Sort by"),
+              ),
+              PopupMenuItem(
+                  child: Text("Delete all"),
+                  onTap: () {
+                    DatabaseHelper.deleteContacts();
+                    setState(() {});
+                  }),
+            ],
+          )
         ],
       ),
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(contacts[index].name + " " + contacts[index].surname),
-          subtitle: Text(contacts[index].phoneNumber),
-        ),
+      body: FutureBuilder(
+        future: DatabaseHelper.getContacts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(data[index].name + " " + data[index].surname),
+                subtitle: Text(data[index].phoneNumber),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ContactDetail(contact: data[index]),
+                      ));
+                },
+              ),
+            );
+          }
+          return Center(child: Text('error'));
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
